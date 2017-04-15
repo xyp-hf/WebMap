ed6dd15e56c7d6efd9bd80bc13e42eb85cd390b1
xyp-hf <2766588380@qq.com>
2017-04-15 21:11:14 +0800

    AngularJS_hz

---
+++ AngularJS/Ch7 AngularJS与服务端交互/data/self.php
@@ -0,0 +1,18 @@
+<?php
+if ($_GET ["action"] == 'search') {
+	if ($_GET ["id"] == '1010')
+		echo "1";
+	else
+		echo "0";
+} elseif ($_GET ["action"] == 'update') {
+	if ($_POST ["key"] == '1011'&&$_GET ["update"] == 'true')
+		echo "1";
+	else
+		echo "0";
+} elseif ($_GET ["action"] == 'save') {
+	if ($_POST ["key"] == '1012')
+		echo "1";
+	else
+		echo "0";
+}
+?>
